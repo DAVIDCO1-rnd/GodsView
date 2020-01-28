6613b99b1da09c71b93a7489449645e03508c6fd
davidco1-rnd <davidco1@rnd-hub.com>
2020-01-28 09:10:25 +0000

    commiting the code for the first time

---
+++ Assets/cameraController.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+    float deltaPosition = 0.1f;
+    float deltaAngle = 1.0f;
+
+    float eps = 0.000001f;
+    // Start is called before the first frame update
+    bool trackingMode;
+    GameObject drone;
+    Vector3 prevDronePosition;
+    Quaternion prevDroneRotation;
+
+    void Start()
+    {
+        drone = GameObject.FindWithTag("drone");
+        trackingMode = false;
+        prevDronePosition = drone.transform.position;
+        prevDroneRotation = drone.transform.rotation;
+    }
+
+    void moveCamera()
+    {
+        //Moving in positive X direction
+        if (Input.GetKey("e"))
+        {
+            transform.position += new Vector3(deltaPosition, 0, 0); 
+        }
+        //Moving in negative X direction
+        if (Input.GetKey("q"))
+        {
+            transform.position += new Vector3(-deltaPosition, 0, 0); 
+        }
+
+        //Moving in positive Y direction
+        if (Input.GetKey("d"))
+        {
+            transform.position += new Vector3(0, deltaPosition, 0); 
+        }
+        //Moving in negative Y direction
+        if (Input.GetKey("a"))
+        {
+            transform.position += new Vector3(0, -deltaPosition, 0); 
+        }
+
+        //Moving in positive Z direction
+        if (Input.GetKey("c"))
+        {
+            transform.position += new Vector3(0, 0, deltaPosition); 
+        }
+        //Moving in negative Z direction
+        if (Input.GetKey("z"))
+        {
+            transform.position += new Vector3(0, 0, -deltaPosition); 
+        } 
+    }
+
+    void rotateCamera()
+    {
+        //Rotating about X axis in positive angle
+        if (Input.GetKey("p"))
+        {
+            transform.Rotate(deltaAngle, 0, 0, Space.Self);
+        }
+        //Rotating about X axis in negative angle
+        if (Input.GetKey("i"))
+        {
+            transform.Rotate(-deltaAngle, 0, 0, Space.Self);
+        }       
+
+        //Rotating about Y axis in positive angle
+        if (Input.GetKey("l"))
+        {
+            transform.Rotate(0, deltaAngle, 0, Space.Self);
+        }
+        //Rotating about Y axis in negative angle
+        if (Input.GetKey("j"))
+        {
+            transform.Rotate(0, -deltaAngle, 0, Space.Self);
+        }
+
+        //Rotating about Z axis in positive angle 
+        if (Input.GetKey("m"))
+        {
+            transform.Rotate(0, 0, deltaAngle, Space.Self);
+        }        
+
+
+        //Rotating about Z axis in negative angle 
+        if (Input.GetKey("b"))
+        {
+            transform.Rotate(0, 0, -deltaAngle, Space.Self);
+        }
+    }
+
+    void trackRotatingDrone()
+    {
+        Quaternion diffRotation = drone.transform.rotation * Quaternion.Inverse(prevDroneRotation);
+        prevDroneRotation = drone.transform.rotation;
+        float angle = 0.0f;
+        Vector3 axis = Vector3.zero;
+        diffRotation.ToAngleAxis(out angle, out axis);         
+        if (trackingMode && angle > eps)
+        {
+            transform.RotateAround(drone.transform.position, axis, angle);                       
+        }
+    }
+
+    void trackMovingDrone()
+    {
+        Vector3 t1 = prevDronePosition - transform.position;
+        Vector3 t2 = drone.transform.position - transform.position;
+        Vector3 movement = t2 - t1;
+        prevDronePosition = drone.transform.position;
+        if (trackingMode && movement.magnitude > eps)
+        {
+            transform.position += movement;                       
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("t"))
+        {
+            trackingMode = !trackingMode;
+        }
+        if (!trackingMode)
+        {
+            moveCamera();
+            rotateCamera();            
+        }
+    }    
+
+    void LateUpdate()
+    {
+        trackRotatingDrone();    
+        trackMovingDrone();
+    }
+}
---
+++ Assets/cubeController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeController : MonoBehaviour
+{
+    float deltaPosition = 0.3f;
+    float deltaAngle = 1.0f;
+
+    void moveCamera()
+    {
+        //Moving in positive X direction
+        if (Input.GetKey("r"))
+        {
+            transform.position += new Vector3(deltaPosition, 0, 0); 
+        }
+        //Moving in negative X direction
+        if (Input.GetKey("w"))
+        {
+            transform.position += new Vector3(-deltaPosition, 0, 0); 
+        }
+
+        //Moving in positive Y direction
+        if (Input.GetKey("f"))
+        {
+            transform.position += new Vector3(0, deltaPosition, 0); 
+        }
+        //Moving in negative Y direction
+        if (Input.GetKey("s"))
+        {
+            transform.position += new Vector3(0, -deltaPosition, 0); 
+        }
+
+        //Moving in positive Z direction
+        if (Input.GetKey("v"))
+        {
+            transform.position += new Vector3(0, 0, deltaPosition); 
+        }
+        //Moving in negative Z direction
+        if (Input.GetKey("x"))
+        {
+            transform.position += new Vector3(0, 0, -deltaPosition); 
+        } 
+    }
+
+    void rotateCamera()
+    {
+        //Rotating about X axis in positive angle
+        if (Input.GetKey("o"))
+        {
+            transform.Rotate(deltaAngle, 0, 0, Space.Self);
+        }
+        //Rotating about X axis in negative angle
+        if (Input.GetKey("u"))
+        {
+            transform.Rotate(-deltaAngle, 0, 0, Space.Self);
+        }       
+
+        //Rotating about Y axis in positive angle
+        if (Input.GetKey("k"))
+        {
+            transform.Rotate(0, deltaAngle, 0, Space.Self);
+        }
+        //Rotating about Y axis in negative angle
+        if (Input.GetKey("h"))
+        {
+            transform.Rotate(0, -deltaAngle, 0, Space.Self);
+        } 
+
+        //Rotating about Z axis in positive angle
+        if (Input.GetKey("n"))
+        {
+            transform.Rotate(0, 0, deltaAngle, Space.Self);
+        }
+        //Rotating about Z axis in negative angle
+        if (Input.GetKey("y"))
+        {
+            transform.Rotate(0, 0, -deltaAngle, Space.Self);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveCamera();
+        rotateCamera();
+    }
+}
